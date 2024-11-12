3a1f44d56d5b21884c82a8a7c6340281e26482cc
soupinsong <a01052050765@gmail.com>
2024-11-12 13:54:09 +0900

    add. test.py iternity

---
+++ test.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("이상이 없지요...??ㅜㅜ")
+print("나는 나 너는 너 사라지지 않아")
